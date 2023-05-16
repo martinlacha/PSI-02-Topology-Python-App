c84d91ea10049f7f9e09f5e8310238119402913f
martinlacha <martin.lacha2@seznam.cz>
2023-05-16 06:49:13 +0200

    Another try to get IP of router

--- src/main.py
+++ src/main.py
@@ -2,44 +2,56 @@ from scapy.all import *
 from scapy.layers.dhcp import BOOTP, DHCP
 from scapy.layers.inet import IP, UDP
 from scapy.layers.l2 import Ether
-
+from pysnmp.hlapi import *
 
 def get_router_ip():
+    # vrátí pole
+    # [0] - interface
+    # [1] 
     print("----------------------------------------")
     gw = conf.route.route('0.0.0.0')
     print(gw)
     print("----------------------------------------")
-    '''
+
+    router_ip = None
+    # Define a DHCP request packet
+    dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff") / IP(src="0.0.0.0", dst="255.255.255.255") / UDP(sport=68, dport=67) / \
+                    BOOTP(chaddr=RandMAC()) / DHCP(options=[("message-type", "discover"), "end"])
+
+    # Send the DHCP request packet and receive the response
+    response = srp(dhcp_discover, timeout=5, verbose=False)
+
+    # Process the DHCP response packets
+    for index, msg in enumerate(response):
+        for packet in response[index]:
+            if DHCP in packet and packet[DHCP].options[0][1] == 2:  # DHCP Offer packet
+                for option in packet[DHCP].options:
+                    if isinstance(option, tuple) and option[0] == 'router':
+                        router_ip = option[1]
+                        break
+
+    return router_ip
+
+def get_ip():
+    router_ip = None
+
     # Create a DHCP discover packet
-    dhcp_discover = Ether(dst='ff:ff:ff:ff:ff:ff') / IP(src='0.0.0.0', dst='255.255.255.255') / UDP(sport=68, dport=67) / BOOTP(chaddr=RandMAC(), xid=RandInt()) / DHCP(options=[('message-type', 'discover'), 'end'])
+    dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff") / IP(src="0.0.0.0", dst="255.255.255.255") / UDP(sport=68, dport=67) / \
+                    BOOTP(chaddr=RandMAC()) / DHCP(options=[("message-type", "discover"), "end"])
 
-    # Send the packet and capture the response
-    dhcp_offer = srp1(dhcp_discover, timeout=2)
+    # Send the DHCP discover packet and receive the response
+    response = srp1(dhcp_discover, timeout=5, verbose=False)
 
-    # Extract the router IP address from the response
-    if dhcp_offer:
-        router_ip = dhcp_offer[BOOTP].siaddr
-        return router_ip
-    else:
-        return None
-    
-    '''
-    # Create a DHCP request packet
-    dhcp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / IP(src="0.0.0.0", dst="255.255.255.255") / UDP(sport=68, dport=67) / BOOTP(chaddr=RandMAC(), xid=RandInt()) / DHCP(options=[("message-type", "request"), ("requested_addr", "0.0.0.0"), "end"])
+    if DHCP in response and response[DHCP].options[0][1] == 2:  # DHCP Offer packet
+        router_ip = response[IP].src
 
-    # Send the packet and capture the response
-    dhcp_offer = srp1(dhcp_request, timeout=10)
-
-    # Extract the router IP address from the response
-    print(dhcp_offer)
-    if dhcp_offer and 'router' in dhcp_offer[DHCP].options:
-        for option in dhcp_offer[DHCP].options:
-            if option[0] == 'router':
-                router_ip = option[1]
-                return router_ip
-    return None
-
+    return router_ip
 
 if __name__ == "__main__":
-    print(get_router_ip())
-    print("----------------------------------------")
+    #print(get_router_ip())
+    print(get_ip())
+
+    # Sniff DHCP packets
+    #pckt = sniff(iface=conf.iface, filter="udp and (port 67 or port 68)", prn=dhcp_packet_handler, store=0)
+    #ip = pckt.getlayer(BOOTP).yiaddr
+    #print(ip)
