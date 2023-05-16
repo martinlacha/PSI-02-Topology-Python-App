bac24f0f27cacf6727bc388d93bfbeca14027d04
martinlacha <martin.lacha2@seznam.cz>
2023-05-16 08:06:19 +0200

    Add SNMP func to get routing table from router

--- src/main.py
+++ src/main.py
@@ -4,16 +4,22 @@ from scapy.layers.inet import IP, UDP
 from scapy.layers.l2 import Ether
 from pysnmp.hlapi import *
 
+router_ip = None
+
+
 def get_router_ip():
+    global router_ip
     # vrátí pole
     # [0] - interface
-    # [1] 
+    # [1] - device IP
+    # [2] - router IP
     print("----------------------------------------")
     gw = conf.route.route('0.0.0.0')
+    router_ip = gw[2]
     print(gw)
     print("----------------------------------------")
 
-    router_ip = None
+    '''
     # Define a DHCP request packet
     dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff") / IP(src="0.0.0.0", dst="255.255.255.255") / UDP(sport=68, dport=67) / \
                     BOOTP(chaddr=RandMAC()) / DHCP(options=[("message-type", "discover"), "end"])
@@ -29,27 +35,57 @@ def get_router_ip():
                     if isinstance(option, tuple) and option[0] == 'router':
                         router_ip = option[1]
                         break
-
-    return router_ip
-
+    '''
+                        
 def get_ip():
     router_ip = None
-
     # Create a DHCP discover packet
     dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff") / IP(src="0.0.0.0", dst="255.255.255.255") / UDP(sport=68, dport=67) / \
                     BOOTP(chaddr=RandMAC()) / DHCP(options=[("message-type", "discover"), "end"])
-
     # Send the DHCP discover packet and receive the response
     response = srp1(dhcp_discover, timeout=5, verbose=False)
-
     if DHCP in response and response[DHCP].options[0][1] == 2:  # DHCP Offer packet
         router_ip = response[IP].src
-
     return router_ip
 
+def find_topology():
+    print("----------------- Topology -----------------")
+    routing_table = []
+    # SNMP parameters
+    snmp_engine = SnmpEngine()
+
+    # SNMP request to retrieve routing table (OID: 1.3.6.1.2.1.4.21.1)
+    var_binds = nextCmd(snmp_engine, CommunityData('public'), UdpTransportTarget((router_ip, 161)),
+                        ContextData(), ObjectType(ObjectIdentity('1.3.6.1.2.1.4.21.1')), lexicographicMode=False)
+
+    # Process SNMP response
+    for error_indication, error_status, error_index, var_bind_table in var_binds:
+        if error_indication:
+            print(f"Error: {error_indication}")
+            return routing_table
+
+        if error_status:
+            print(f"Error: {error_status.prettyPrint()}")
+            return routing_table
+
+        # Extract routing table information
+        for var_bind in var_bind_table:
+            oid = var_bind[0]
+            value = var_bind[1]
+
+            # Process each entry in the routing table
+            if str(oid).startswith('1.3.6.1.2.1.4.21.1.7'):  # OID for routing table entry
+                index = oid.split('.')[-1]
+                route_entry = f"Index: {index}, Next Hop: {value}"
+                routing_table.append(route_entry)
+
+    print("--------------------------------------------")
+    print(routing_table)
+
+
 if __name__ == "__main__":
-    #print(get_router_ip())
-    print(get_ip())
+    get_router_ip()
+    find_topology()
 
     # Sniff DHCP packets
     #pckt = sniff(iface=conf.iface, filter="udp and (port 67 or port 68)", prn=dhcp_packet_handler, store=0)
