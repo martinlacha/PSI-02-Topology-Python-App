7ad07973301dab2d5e3e0c30b92689315baff697
martinlacha <martin.lacha2@seznam.cz>
2023-05-16 10:58:26 +0200

    Fix wrong var name

--- src/main.py
+++ src/main.py
@@ -73,7 +73,7 @@ def find_topology():
                         ContextData(), ObjectType(ObjectIdentity('1.3.6.1.2.1.4.21.1')), lexicographicMode=False)
 
     print(f"-----------------------var_binds---------------------------")
-    print(f"{var_bind}")
+    print(f"{var_binds}")
     # Process SNMP response
     for error_indication, error_status, error_index, var_bind_table in var_binds:
         if error_indication:
