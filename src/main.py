012c3f349a5d4f5569fb688490392d12430e03c1
martinlacha <martin.lacha2@seznam.cz>
2023-05-24 12:29:12 +0200

    test3

--- src/main.py
+++ src/main.py
@@ -112,7 +112,7 @@ def get_interface_ips(ip):
                CommunityData(community),
                UdpTransportTarget((ip, 161)),
                ContextData(),
-               ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr')))
+               ObjectType(ObjectIdentity('1.3.6.1.2.1.4.20')))
     )
 
     if error_indication:
