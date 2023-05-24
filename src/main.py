fecaddc0c1b29cb3f09db0c7f64e4f95bec1cb9d
martinlacha <martin.lacha2@seznam.cz>
2023-05-24 12:32:49 +0200

    test7

--- src/main.py
+++ src/main.py
@@ -112,7 +112,7 @@ def get_interface_ips(ip):
                CommunityData(community),
                UdpTransportTarget((ip, 161)),
                ContextData(),
-               ObjectType(ObjectIdentity('1.3.6.1.2')))
+               ObjectType(ObjectIdentity('1.3.6.1.2.1.1.2.0')))
     )
 
     if error_indication:
