0f4939af70e634e13e6d18419ef88bbf1453ee49
martinlacha <martin.lacha2@seznam.cz>
2023-05-26 11:28:17 +0200

    Remake get interface function

--- src/main.py
+++ src/main.py
@@ -85,51 +85,23 @@ def get_routing_table(router_ip):
     return routing_table
 
 
-'''
-# Get system ID by IP address
-def get_system_id(ip):
-    error_indication, error_status, error_index, var_binds = next(
-        getCmd(SnmpEngine(),
-               CommunityData(community),
-               UdpTransportTarget((ip, 161)),
-               ContextData(),
-               ObjectType(ObjectIdentity('1.3.6.1.2.1.1.2.0')))
-    )
-    if error_indication:
-        print(f"Chyba: {error_indication}")
-    elif error_status:
-        print(f"Chyba: {error_status.prettyPrint()} na indexu {error_index and var_binds[int(error_index) - 1][0] or '?'}")
-    else:
-        for var_bind in var_binds:
-            print(f"System ID: {var_bind.prettyPrint()}")
-'''
-
-
 # Get interfaces IPs
 def get_interface_ips(ip):
-    error_indication, error_status, error_index, var_binds = next(
-        getCmd(SnmpEngine(),
-               CommunityData(community),
-               UdpTransportTarget((ip, 161)),
-               ContextData(),
-               ObjectType(ObjectIdentity('1.3.6.1.2.1.4.20.1.1')), lexicographicMode=False)
-    )
+    for (errorIndication,errorStatus,errorIndex,varBinds) in nextCmd(SnmpEngine(),
+        CommunityData(community), UdpTransportTarget((ip, 161)), ContextData(),
+        ObjectType(ObjectIdentity('1.3.6.1.2.1.4.20.1.1')), lexicographicMode=False):
 
-    if error_indication:
-        print(f"Error: {error_indication}")
-        return
-    elif error_status:
-        print(f"Error: {error_status.prettyPrint()} at index {error_index and var_binds[int(error_index) - 1][0] or '?'}")
-        return
-
-    print(f"var_binds: {var_binds}")
-    for var_bind in var_binds:
-        print(f"var_bind: {var_bind}")
-        '''
-        for var in var_bind:
-            interface_ip = var[1]
-            print(f"IP Address: {interface_ip}")
-        '''
+        if errorIndication:
+            print(errorIndication, file=sys.stderr)
+            break
+        elif errorStatus:
+            print('%s at %s' % (errorStatus.prettyPrint(),
+                                errorIndex and varBinds[int(errorIndex) - 1][0] or '?'),
+                                file=sys.stderr)
+            break
+        else:
+            for varBind in varBinds:
+                print(f"interface: {varBind}")
 
 
 # Get hostname of router by IP address
