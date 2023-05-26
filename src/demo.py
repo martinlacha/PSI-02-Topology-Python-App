17115608348204530278d0b554b1f4f63ec93d92
martinlacha <martin.lacha2@seznam.cz>
2023-05-26 11:13:39 +0200

    add test file

--- src/demo.py
+++ src/demo.py
@@ -1,23 +1,20 @@
-
 from pysnmp.hlapi import *
-
-iterator = getCmd(
-    SnmpEngine(),
-    CommunityData('public', mpModel=0),
-    UdpTransportTarget(('demo.snmplabs.com', 161)),
-    ContextData(),
-    ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
-)
-
-errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
-
-if errorIndication:
-    print(errorIndication)
-
-elif errorStatus:
-    print('%s at %s' % (errorStatus.prettyPrint(),
-                        errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-
-else:
-    for varBind in varBinds:
-        print(' = '.join([x.prettyPrint() for x in varBind]))
+ 
+def walk(host, oid):
+    for (errorIndication,errorStatus,errorIndex,varBinds) in nextCmd(SnmpEngine(),
+        CommunityData('PSIPUB'), UdpTransportTarget((host, 161)), ContextData(),
+        ObjectType(ObjectIdentity(oid)), lexicographicMode=False):
+        if errorIndication:
+            print(errorIndication, file=sys.stderr)
+            break
+        elif errorStatus:
+            print('%s at %s' % (errorStatus.prettyPrint(),
+                                errorIndex and varBinds[int(errorIndex) - 1][0] or '?'),
+                                file=sys.stderr)
+            break
+        else:
+            for varBind in varBinds:
+                print(varBind)
+ 
+#walk('10.0.2.254', '1.3.6.1.2.1.4.24.4.1.1')
+walk('192.168.1.2', '1.3.6.1.2.1.4.20.1.1')
